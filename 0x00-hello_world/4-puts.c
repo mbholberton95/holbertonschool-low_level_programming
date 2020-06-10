13ce9e351144aaa5dd3233eae61d41b637b16990
mbholberton95 <1984@holbertonschool.com>
2020-06-10 15:24:14 -0500

    Gear fouuuuuurth Snakeuman

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#!/bin/bash
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Programming is like building a multilingual puzzle\b");
+   return 0;
+}
