4f402c9096ea7c39907a958e90725462bd3aa076
mbholberton95 <1984@holbertonschool.com>
2020-06-10 16:38:37 -0500

    5-printf2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,16 +2,9 @@
 int main (void)
   /**
  * main - Printf is going to print the string
- * Return: Always (0).
+ * Return: Always (0)
  */ 
-
-  
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return(0);
 }
-
-
-
-
-
