5c3cbea9169335278d0041f28b0f5f0afe19bbfb
mbholberton95 <1984@holbertonschool.com>
2020-06-10 15:50:09 -0500

    snakemaan

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main(void) {
-  char *s = "\"Programming is like building a multilingual puzzle\n";
-  puts(s);
-  return(0);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
