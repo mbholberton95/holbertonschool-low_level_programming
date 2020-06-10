d551fd6bc2707ad3161d54e096e833e3fa346ee1
mbholberton95 <1984@holbertonschool.com>
2020-06-10 16:32:47 -0500

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (void)
+  /**
+ * main - Printf is going to print the string
+ * Return: Always (0).
+ */ 
+
+  
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return(0);
+}
+
+
+
+
+
