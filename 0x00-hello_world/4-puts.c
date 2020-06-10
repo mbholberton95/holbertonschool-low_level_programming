c97d43deb11bfea7c4b075a9f7407afe07a2ca25
mbholberton95 <1984@holbertonschool.com>
2020-06-10 15:54:38 -0500

    snakemaaaaaaaan

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,6 @@ int main(void)
   * Return: Always 0 (Sucess)
 */
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
