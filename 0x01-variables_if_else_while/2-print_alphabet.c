6abd68dbdb384b1ea13fa3e660a1234969f39bfe
mbholberton95 <1853@holbertonschool.com>
2020-06-12 16:50:21 -0500

    alphabet1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Will print abc
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+    char i;
+    for (i='a'; i<='z'; i++)
+    putchar(i);
+    putchar('\n');
+    return (0);
+  }
